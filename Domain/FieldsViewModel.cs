c67eec15ecd15d303e6c8485eee9fb71849b3cb4
Hygge <lisok1130@gmail.com>
2023-07-09 23:26:31 +0800

    feat:构建用户管理界面

---
+++ Domain/FieldsViewModel.cs
@@ -0,0 +1,319 @@
+﻿using System.Collections.ObjectModel;
+
+namespace hygge_imaotai.Domain
+{
+    /// <summary>
+    /// 用户管理 - 搜索的condition
+    /// </summary>
+    public class FieldsViewModel:ViewModelBase
+    {
+        private string? _phone;
+        private string? _userId;
+        private string? _province;
+        private string? _city;
+
+        public FieldsViewModel()
+        {
+            SearchResult = CreateData();
+        }
+
+        private static ObservableCollection<SelectableViewModel> CreateData()
+        {
+            return new ObservableCollection<SelectableViewModel>
+            {
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Food = "Fries"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it"
+                },
+            };
+        }
+
+        public string? Phone
+        {
+            get => _phone;
+            set => SetProperty(ref _phone, value);
+        }
+
+        public string? UserId
+        {
+            get => _userId;
+            set => SetProperty(ref _userId, value);
+        }
+
+        public string? Province
+        {
+            get => _province;
+            set => SetProperty(ref _province, value);
+        }
+
+        public string? City
+        {
+            get => _city;
+            set => SetProperty(ref _city, value);
+        }
+
+        public ObservableCollection<SelectableViewModel> SearchResult { get; }
+    }
+}
---
+++ UserManageControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using hygge_imaotai.Domain;
+
+namespace hygge_imaotai
+{
+    /// <summary>
+    /// UserManageControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserManageControl : UserControl
+    {
+        public UserManageControl()
+        {
+            InitializeComponent();
+            DataContext = new FieldsViewModel();
+        }
+    }
+}
